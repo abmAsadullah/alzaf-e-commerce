f9e45d4862f7182f25de4c32c3a9a20d7be680c1
A B M Asadullah <abmasadullah@gmail.com>
2024-11-07 12:27:51 +0600

    Registration form start

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,16 @@
-import Image from "next/image";
+import Image from 'next/image'
 
 export default function Home() {
   return (
-    <>
-      BODY
-    </>
+    <div>
+       <Image
+        src="/banner.png"
+        width={0}
+        height={0}
+        sizes="100vw"
+        style={{ width: '100%', height: 'auto' }}
+        alt="Banner"
+      />
+    </div>
   );
 }
