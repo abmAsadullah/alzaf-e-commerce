1f4336515d768670189472cc58a2979330b6c39d
A B M Asadullah <abmasadullah@gmail.com>
2024-11-09 00:32:43 +0600

    Register page

--- app/layout.tsx
+++ app/layout.tsx
@@ -27,7 +27,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased bg-gray-300 h-svh`}
+        className={`${geistSans.variable} ${geistMono.variable} antialiased bg-gray-100 h-svh`}
       >
         <Navigation />
         {children}
